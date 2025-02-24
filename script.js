3c1651a4d0a60a3b08518d31873fb0cd56f58b71
Ivan Russel <russelblanquiza@gmail.com>
2025-02-24 10:39:49 +0800

    Encapsulate game status in global variable in a constant gameState object.

--- script.js
+++ script.js
@@ -1,177 +1,184 @@
-let playerPoints = 0;
-let computerPoints = 0;
-let roundCount = 0;
-let isPopUpActive = false;
+const gameState = {
+  playerPoints: 0,
+  computerPoints: 0,
+  roundCount: 0,
+  isPopUpActive: false,
+};
 
 const playerSide = document.querySelector(".player-side");
 const computerSide = document.querySelector(".computer-side");
-const heading = document.querySelector('.header-title');
-const scoreBoard = document.querySelector('.score-board');
+const heading = document.querySelector(".header-title");
+const scoreBoard = document.querySelector(".score-board");
 const popUpContainer = document.querySelector(".pop-up-container");
-const buttonContainer = document.querySelector(".button-container")
+const buttonContainer = document.querySelector(".button-container");
 
 playGame();
 
+function resetGameState() {
+  gameState.playerPoints = 0;
+  gameState.computerPoints = 0;
+  gameState.roundCount = 0;
+  gameState.isPopUpActive = false;
+  scoreBoard.textContent = "0 - 0";
+}
+
 function playAgainPopUpBox(message) {
-    const playAgainBox = document.createElement("div");
-    const playAgainButton = document.createElement("button");
-    playAgainBox.classList.add("pop-up-box");
-    playAgainBox.textContent = message;
-    playAgainButton.classList.add("continue-button");
-    playAgainButton.textContent = "Play Again";
+  const playAgainBox = document.createElement("div");
+  const playAgainButton = document.createElement("button");
+  playAgainBox.classList.add("pop-up-box");
+  playAgainBox.textContent = message;
+  playAgainButton.classList.add("continue-button");
+  playAgainButton.textContent = "Play Again";
 
-    popUpContainer.appendChild(playAgainBox);
-    playAgainBox.appendChild(playAgainButton);
+  popUpContainer.appendChild(playAgainBox);
+  playAgainBox.appendChild(playAgainButton);
 
-    playAgainButton.addEventListener('click', () => {
-        isPopUpActive = false;
-        roundCount = 0; 
-        computerPoints = 0;
-        playerPoints = 0;
-        scoreBoard.textContent = "0 - 0";
-        clearScreen();
-        playGame();
-    });
+  playAgainButton.addEventListener("click", () => {
+    resetGameState();
+    clearScreen();
+    playGame();
+  });
 }
 
 function showWinnerBox(message) {
-    if(playerPoints === 5 || computerPoints === 5) return;
-    const popUpBox = document.createElement('div');
-    const continueButton = document.createElement('button');
-    popUpBox.classList.add("pop-up-box");
-    continueButton.classList.add("continue-button");
-    continueButton.textContent = "Continue"
-    popUpBox.textContent = message;
-    popUpContainer.appendChild(popUpBox);
-    popUpBox.appendChild(continueButton);
-    isPopUpActive = true;
+  if (gameState.playerPoints === 5 || gameState.computerPoints === 5) return;
+  const popUpBox = document.createElement("div");
+  const continueButton = document.createElement("button");
+  popUpBox.classList.add("pop-up-box");
+  continueButton.classList.add("continue-button");
+  continueButton.textContent = "Continue";
+  popUpBox.textContent = message;
+  popUpContainer.appendChild(popUpBox);
+  popUpBox.appendChild(continueButton);
+  isPopUpActive = true;
 
-    continueButton.addEventListener("click", () => {
-        isPopUpActive = false;
-        clearScreen();
-    });
+  continueButton.addEventListener("click", () => {
+    isPopUpActive = false;
+    clearScreen();
+  });
 }
 
 function simulateActionLoadingScreen() {
-    const computerDotLoader = document.createElement("div");
-    computerDotLoader.classList.add("dot-loader");
-    computerSide.appendChild(computerDotLoader);
+  const computerDotLoader = document.createElement("div");
+  computerDotLoader.classList.add("dot-loader");
+  computerSide.appendChild(computerDotLoader);
 
-    return new Promise(() => {
-        setTimeout(() =>{
-            computerSide.removeChild(computerSide.lastChild);
-        }, 2000);
-    });
+  return new Promise(() => {
+    setTimeout(() => {
+      computerSide.removeChild(computerSide.lastChild);
+    }, 2000);
+  });
 }
 
 function displayImageToScreen(screen, asset) {
-    image = document.createElement('img');
-    if(!screen.hasChildNodes()) {  
-        image.setAttribute('src', asset)
-        screen.appendChild(image);
-    }
+  image = document.createElement("img");
+  if (!screen.hasChildNodes()) {
+    image.setAttribute("src", asset);
+    screen.appendChild(image);
+  }
 }
 
 function clearScreen() {
-    if(playerSide.hasChildNodes() || computerSide.hasChildNodes()) {
-        playerSide.removeChild(playerSide.lastChild);
-        computerSide.removeChild(computerSide.lastChild);
-    }
-    popUpContainer.removeChild(popUpContainer.lastChild);
+  if (playerSide.hasChildNodes() || computerSide.hasChildNodes()) {
+    playerSide.removeChild(playerSide.lastChild);
+    computerSide.removeChild(computerSide.lastChild);
+  }
+  popUpContainer.removeChild(popUpContainer.lastChild);
 }
 
 async function getPlayerChoice() {
-    return new Promise((choice) => {
-        buttonContainer.addEventListener('click', (e) => {
-            if(isPopUpActive) return;
-            
-           
-            let target = e.target;
-            switch(target.id) {
-                case 'rock':
-                    displayImageToScreen(playerSide, "assets/rock.png");
-                    choice("ROCK");
-                    break;
-                case 'paper':
-                    displayImageToScreen(playerSide, "assets/paper.png");
-                    choice("PAPER");
-                    break;
-                case 'scissor':
-                    displayImageToScreen(playerSide, "assets/scissors.png");
-                    choice("SCISSOR");
-                    break;
-            }
-        });
+  return new Promise((choice) => {
+    buttonContainer.addEventListener("click", (e) => {
+      if (gameState.isPopUpActive) return;
+
+      let target = e.target;
+      switch (target.id) {
+        case "rock":
+          displayImageToScreen(playerSide, "assets/rock.png");
+          choice("ROCK");
+          break;
+        case "paper":
+          displayImageToScreen(playerSide, "assets/paper.png");
+          choice("PAPER");
+          break;
+        case "scissor":
+          displayImageToScreen(playerSide, "assets/scissors.png");
+          choice("SCISSOR");
+          break;
+      }
     });
+  });
 }
 
 function roundResult(choice) {
-    switch(choice) {
-        case "ROCK": return "Rock beats Scissors"
-        case "PAPER": return "Paper beats Rock"
-        case "SCISSOR": return "Scissors beats Paper"
-    }
+  switch (choice) {
+    case "ROCK":
+      return "Rock beats Scissors";
+    case "PAPER":
+      return "Paper beats Rock";
+    case "SCISSOR":
+      return "Scissors beats Paper";
+  }
 }
 
 function getComputerChoice() {
-    let randomNumber = Math.floor((Math.random() * 3) + 1);
-    
-    switch(randomNumber) {
-        case 1: 
-            displayImageToScreen(computerSide, "assets/rock.png");
-            return "ROCK";
-        case 2: 
-            displayImageToScreen(computerSide, "assets/paper.png");
-            return "PAPER";
-        case 3: 
-            displayImageToScreen(computerSide, "assets/scissors.png");
-            return "SCISSOR";
-    }
+  let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+  switch (randomNumber) {
+    case 1:
+      displayImageToScreen(computerSide, "assets/rock.png");
+      return "ROCK";
+    case 2:
+      displayImageToScreen(computerSide, "assets/paper.png");
+      return "PAPER";
+    case 3:
+      displayImageToScreen(computerSide, "assets/scissors.png");
+      return "SCISSOR";
+  }
 }
 
 async function playRound() {
-    let playerChoice = await getPlayerChoice();
-    simulateActionLoadingScreen();
-    await new Promise(resolve => setTimeout(resolve, 2000)); 
-    let computerChoice = await getComputerChoice();
-    await new Promise(resolve => setTimeout(resolve, 1200)); 
+  let playerChoice = await getPlayerChoice();
+  simulateActionLoadingScreen();
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  let computerChoice = await getComputerChoice();
+  await new Promise((resolve) => setTimeout(resolve, 1200));
 
-   if(playerChoice === computerChoice) {
-        showWinnerBox('Draw!');
-        return;
-    }
-    else if((playerChoice === "ROCK" && computerChoice === "SCISSOR") ||
-            (playerChoice === "PAPER" && computerChoice === "ROCK") || 
-            (playerChoice === "SCISSOR" && computerChoice === "PAPER")) {
+  if (playerChoice === computerChoice) {
+    showWinnerBox("Draw!");
+    return;
+  } else if (
+    (playerChoice === "ROCK" && computerChoice === "SCISSOR") ||
+    (playerChoice === "PAPER" && computerChoice === "ROCK") ||
+    (playerChoice === "SCISSOR" && computerChoice === "PAPER")
+  ) {
+    gameState.playerPoints++;
+    showWinnerBox(`You Win! ${roundResult(playerChoice)}`);
+  } else {
+    gameState.computerPoints++;
+    showWinnerBox(`You Lose! ${roundResult(computerChoice)}`);
+  }
+  scoreBoard.textContent = `${gameState.playerPoints} - ${gameState.computerPoints}`;
 
-        playerPoints++;       
-        showWinnerBox(`You Win! ${roundResult(playerChoice)}`);
-    }
-    else {
-        computerPoints++;
-        showWinnerBox(`You Lose! ${roundResult(computerChoice)}`);
-    }
-    scoreBoard.textContent = `${playerPoints} - ${computerPoints}`;
-
-    roundCount++;
-    //Logging
-    console.log(`Round ${roundCount} result - You: ${playerChoice} | Computer: ${computerChoice}`);
+  gameState.roundCount++;
+  //Logging
+  console.log(
+    `Round ${gameState.roundCount} result - You: ${playerChoice} | Computer: ${computerChoice}`
+  );
 }
 
 async function playGame() {
-    while(roundCount <= 10) {
-       await playRound();
-       if(playerPoints === 5 || computerPoints === 5) break;
-    }   
+  while (gameState.roundCount <= 10) {
+    await playRound();
+    if (gameState.playerPoints === 5 || gameState.computerPoints === 5) break;
+  }
 
-    if(playerPoints > computerPoints) {
-       playAgainPopUpBox("Player Wins!");
-    }
-    else {
-        playAgainPopUpBox("Computer Wins!");
-    }
+  if (gameState.playerPoints > gameState.computerPoints) {
+    playAgainPopUpBox("Player Wins!");
+  } else {
+    playAgainPopUpBox("Computer Wins!");
+  }
 
-    console.log(`Human Score: ${playerPoints}`); // for logging
-    console.log(`Computer Score: ${computerPoints}`);
+  console.log(`Human Score: ${gameState.playerPoints}`); // for logging
+  console.log(`Computer Score: ${gameState.computerPoints}`);
 }
-
